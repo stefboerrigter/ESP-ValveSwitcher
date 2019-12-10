f4a0f74e81042532c4512ffb5ce1edcda1bd3449
Stef Boerrigter <stefboerrigter@gmail.com>
2019-12-10 20:50:50 +0100

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ *  This sketch demonstrates how to scan WiFi networks. 
+ *  The API is almost the same as with the WiFi Shield library, 
+ *  the most obvious difference being the different file you need to include:
+ */
+#include "ESP8266WiFi.h"
+#include <Arduino.h>
+#include "tempsensor.h"
+
+#define WIFI_SSID "FamBoerrigter"
+#define WIFI_PWD  "BoerrigterGijsbers147"
+
+DS18 ds18;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
